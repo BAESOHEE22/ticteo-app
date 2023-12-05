94ffa323826594d98f78b91ee6185cbb79f8449b
SOHEE <hyian22@naver.com>
2023-12-05 14:50:15 +0900

    처음처럼

---
+++ src/components/Square.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Square.css'
+
+const Square = ({onClick, value}) => {
+  
+    return (
+      <button className="Square" onClick={() => onClick()}>
+           {value}
+      </button>
+    )
+ }
+ export default Square
